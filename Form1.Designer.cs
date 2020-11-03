d749594707f60e5250ca63467cc39c3b788b95a0
einzluis <einzluisboticario@gmail.com>
2020-11-03 22:42:05 +0800

    Hopia!

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace FaceDetectionCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxDevice = new System.Windows.Forms.ComboBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnDetect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device";
+            // 
+            // cbxDevice
+            // 
+            this.cbxDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxDevice.FormattingEnabled = true;
+            this.cbxDevice.Location = new System.Drawing.Point(157, 13);
+            this.cbxDevice.Margin = new System.Windows.Forms.Padding(4);
+            this.cbxDevice.Name = "cbxDevice";
+            this.cbxDevice.Size = new System.Drawing.Size(814, 24);
+            this.cbxDevice.TabIndex = 1;
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(157, 69);
+            this.pic.Margin = new System.Windows.Forms.Padding(4);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(889, 442);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            this.pic.Click += new System.EventHandler(this.Pic_Click);
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(23, 69);
+            this.btnDetect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(117, 28);
+            this.btnDetect.TabIndex = 3;
+            this.btnDetect.Text = "&Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.BtnDetect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.cbxDevice);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hopia";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Pic_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxDevice;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnDetect;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceDetectionCamera
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Choices list = new Choices();
+        public Form1()
+        {
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+
+            list.Add(new String[] { "hello", "how are you", "what is your name", "what are you", "what time is it", "what is the date today" });
+
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gr);
+                rec.SpeechRecognized += rec_SpeechRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                return;
+            }
+
+            //SET VOICE, TEST
+            s.SelectVoiceByHints(VoiceGender.Female);
+            s.Speak("Hello! My name is Hopia");
+            InitializeComponent();
+        }
+        public void say(string h)
+        {
+            s.Speak(h);
+        }
+        private void rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String r = e.Result.Text;
+
+            /* Here are some pre-made responses. 
+             * I know it's few. 
+             * Feel free to add more...
+             * 
+             What you say */
+            if (r == "hello")
+            { //What it says
+                say("hi");
+                return;
+            }
+            //What you say
+            if (r == "how are you")
+            { //What it says
+                say("great, how about you");
+                return;
+            }
+            if (r == "what time is it")
+            {
+                say(DateTime.Now.ToString("h:mm tt"));
+                return;
+            }
+            if (r == "what is the date today")
+            {
+                say(DateTime.Now.ToString("M/d/yyyy"));
+                return;
+            }
+            //What you say
+            if (r == "what is your name")
+            { //What it says
+                say("my name is Hopia");
+                return;
+            }
+            //What you say
+            if (r == "what are you")
+            { //What it says
+                say("im a program, please feed me more information");
+                return;
+            }
+
+        }
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+                cbxDevice.Items.Add(device.Name);
+            cbxDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+        }
+
+        private void BtnDetect_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[cbxDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            foreach(Rectangle rectangle in rectangles)
+            {
+                using(Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 5))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            pic.Image = bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (device.IsRunning)
+                device.Stop();
+        }
+    }
+}
